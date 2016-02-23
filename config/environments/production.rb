ab9cf6302bab6417d2df4c42a9fc2549de04599e
Kelsey James <kelseybjames@gmail.com>
2016-02-23 14:10:12 -0800

    adding mailer to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.raise_delivery_errors = false
+
+  # Test mail with letter_opener
+  config.action_mailer.delivery_method = :letter_opener
 end
