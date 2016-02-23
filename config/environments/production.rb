31fbfe8e93a7714e26dc41aec708fb3e1d5c2ae6
Kelsey James <kelseybjames@gmail.com>
2016-02-23 14:20:03 -0800

    adding config options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,8 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.raise_delivery_errors = false
-
-  # Test mail with letter_opener
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = {
+    :host => 'https://gentle-coast-52610.herokuapp.com',
+  }
 end
