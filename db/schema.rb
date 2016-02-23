50efbae6b417544d32b377e4921fd39499dd8dbd
Kelsey James <kelseybjames@gmail.com>
2016-02-23 14:29:38 -0800

    added procfile

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160223170836) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer  "priority",   default: 0, null: false
     t.integer  "attempts",   default: 0, null: false
@@ -30,7 +27,7 @@ ActiveRecord::Schema.define(version: 20160223170836) do
     t.datetime "updated_at"
   end
 
-  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
