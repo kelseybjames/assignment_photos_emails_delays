3c982089ed15d6783597158c1f0599c901c7bb77
Kelsey James <kelseybjames@gmail.com>
2016-02-23 08:46:11 -0800

    generated mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,5 @@ class User < ActiveRecord::Base
 
 
   validates_attachment_content_type :profile_photo, :content_type => /\Aimage\/.*\Z/
-  # def profile_photo=(profile_photo)
-  #   self.profile_photo_data = profile_photo.read
-  #   self.profile_photo_filename = profile_photo.original_filename
-  #   self.profile_photo_mime_type = profile_photo.content_type
-  # end
 end
 
